82e008df0d6c009ac613746541f0f5c49b1291bd
PranavHegde <hegdepanu4@gmail.com>
2020-08-30 16:07:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>Here be the App of Aws Amplify Deploying</code> and save to reload.
         </p>
       </div>
     );
